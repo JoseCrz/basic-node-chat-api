6045391c8695cf061ad21435975fbc227a2322b2
Jose Cuevas <josecueram@gmail.com>
2020-03-25 20:56:09 -0600

    basic express router implementation

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const router = express.Router()
 
 const app = express()
 
-app.use('/', (request, response) => {
+router.get('/', (request, response) => {
     response.send('Hello world!')
 })
+
+router.get('/message', (request, response) => {
+    response.send('List of messages...')
+})
+
+router.post('/message', (request, response) => {
+    response.send('Message received!')
+})
+
+app.use(router)
 
 console.log('Listening on port 3000')
 app.listen(3000)
