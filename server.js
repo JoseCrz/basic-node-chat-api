b60be4bd8d994ae44b8080abba3887c5253babbd
Jose Cuevas <josecueram@gmail.com>
2020-03-30 21:16:25 -0600

    integrated socket.io in main server file

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const server = require('http').Server(app)
 const bodyParser = require('body-parser')
 
+const socket = require('./socket')
 const routes = require('./network/routes')
 const db = require('./db')
 
@@ -11,6 +12,7 @@ db.connect()
 // SECTION App configuration
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+socket.connect(server)
 
 // Load routes to the app
 routes(app)
